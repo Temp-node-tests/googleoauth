31366101b294ae3daf298dc128c0c02a5235e17a
sasikanth <sasikanth.b.009@gmail.com>
2020-05-28 23:42:32 +0530

    firstt

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require("express");
-const app = express()
+const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log("c: refreshToken", refreshToken);
+      console.log("c: profile", profile);
+    }
+  )
+);
 
-app.get('/',(req,res) => {
-    res.send('gg');
-})
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'><button>sign in</button></a>");
+});
 
-app.listen(PORT,() => {
-    console.log(`running on ${PORT}`);
-})
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
---
+++ ruff.js
@@ -0,0 +1,3 @@
+https://accounts.google.com/signin/oauth/
+oauthchooseaccount?response_type=code&
+redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=914681712936-jbhf8heiincomtajmpeh3ff50959tgtj.apps.googleusercontent.com&o2v=2&as=XmX3XjXf7AjaFji_LX_Ogw&flowName=GeneralOAuthFlow
